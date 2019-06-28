ed9c49df6f69082e9903be6aa20bb7d8ed569b24
Aad Versteden <madnificent@gmail.com>
2019-06-28 16:55:56 +0200

    First producer service PoC
    The first PoC is a first variant which could possibly be used to sync
    some data between applications.

--- app.js
+++ app.js
@@ -1,7 +1,49 @@
-# see https://github.com/mu-semtech/mu-javascript-template for more info
+// see https://github.com/mu-semtech/mu-javascript-template for more info
 
 import { app, query, errorHandler } from 'mu';
+import fs from 'fs';
+import bodyParser from 'body-parser';
 
-app.get('/', function( req, res ) {
-  res.send('Hello mu-javascript-template');
+app.use( bodyParser.json( { type: function(req) { return /^application\/json/.test( req.get('content-type') ); } } ) );
+
+const DELTA_INTERVAL = 1000;
+
+let cache = [];
+let hasTimeout = null;
+
+app.post('/delta', function( req, res ) {
+  const body = req.body;
+
+  console.log(`Pushing onto cache ${JSON.stringify(body)}`);
+
+  cache.push( ...body );
+
+  if( !hasTimeout ){
+    triggerTimeout();
+  }
+
+  res.status(200).send("Processed");
 } );
+
+
+function triggerTimeout(){
+  setTimeout( generateDeltaFile, DELTA_INTERVAL );
+  hasTimeout = true;
+
+}
+
+function generateDeltaFile(){
+  const cachedArray = cache;
+  cache = [];
+
+  hasTimeout = false;
+
+  fs.writeFile(`/data/delta-${new Date().toISOString()}`, JSON.stringify( cachedArray ), function(err) {
+    if(err) {
+      return console.log(err);
+    }
+
+    console.log("The file was saved!");
+    return null;
+  });
+}
